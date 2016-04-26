985b03c70e98bf9c4e10d0b2c871124b7147a1fd
ken <cheekian@foxmail.com>
2016-04-26 14:35:10 +0800

    mib

--- assets/controllers/easywash.js
+++ assets/controllers/easywash.js
@@ -70,7 +70,7 @@ function($scope, $http){
           alert("init success");
           //alert($scope.shareCount);
           //$scope.drawChance = data.drawChance;
-          //alert("user drawChance: "+$scope.drawChance);
+          alert($scope.timestamp+" "+$scope.noncestr+" "+$scope.signature);
           wx.config({
           debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: 'wxab261de543656952', // 必填，公众号的唯一标识
