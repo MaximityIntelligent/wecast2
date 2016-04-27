4cc6bc559a552b6d3897bb49d4dc238ea2a525c4
ken <cheekian@foxmail.com>
2016-04-27 15:07:51 +0800

    mib

--- assets/controllers/easywash.js
+++ assets/controllers/easywash.js
@@ -72,7 +72,7 @@ function($scope, $http){
           //$scope.drawChance = data.drawChance;
           //alert($scope.timestamp+" "+$scope.noncestr+" "+$scope.signature);
           wx.config({
-          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: 'wxab261de543656952', // 必填，公众号的唯一标识
           timestamp: $scope.timestamp, // 必填，生成签名的时间戳
           nonceStr: $scope.noncestr, // 必填，生成签名的随机串
@@ -160,7 +160,6 @@ function($scope, $http){
   }
 
   $scope.showPrize1 = function(){
-    alert("2");
     if($scope.credit>18){
       return true;
     }else{
