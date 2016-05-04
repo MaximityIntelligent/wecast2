028e64d68729f6ea98ab9874cdafb38805d5aa47
ken <cheekian@foxmail.com>
2016-05-04 15:58:25 +0800

    mib

--- config/connections.js
+++ config/connections.js
@@ -83,13 +83,6 @@ module.exports.connections = {
   *                                                                          *
   *                                                                          *
   ***************************************************************************/
-  somePostgresqlServer: {
-    adapter: 'sails-postgresql',
-    host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_POSTGRES_USER',
-    password: 'YOUR_POSTGRES_PASSWORD',
-    database: 'YOUR_POSTGRES_DB'
-  }
 
 
   /***************************************************************************
