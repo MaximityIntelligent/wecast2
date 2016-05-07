01d86da36e8cb2d3ce107c3c57919be2ac544eae
ken <cheekian@foxmail.com>
2016-05-07 13:57:35 +0800

    mib

--- assets/controllers/easywash.js
+++ assets/controllers/easywash.js
@@ -57,7 +57,7 @@ function($scope, $http){
       $scope.$apply(function(){
           $scope.prevPage = $scope.atPage;
           $scope.atPage = $scope.LANDSCAPE;
-          $("#landscapeModal").modal('show');
+          //$("#landscapeModal").modal('show');
       });
       $('body').removeClass('portrait');
     }
@@ -92,7 +92,7 @@ function($scope, $http){
           $scope.shareCount = data.shareCount;
           $scope.credit = data.credit;
           wx.config({
-          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: 'wxab261de543656952', // 必填，公众号的唯一标识
           timestamp: $scope.timestamp, // 必填，生成签名的时间戳
           nonceStr: $scope.noncestr, // 必填，生成签名的随机串
