aa4d5aa6f900748872bf7c1aa357d574c4ecc7b2
ken <cheekian@foxmail.com>
2016-05-23 14:10:42 +0800

    mib

--- api/controllers/LogController.js
+++ api/controllers/LogController.js
@@ -16,9 +16,15 @@ module.exports = {
 		});
 	},
 	find: function(req, res){
-		//var action = req.param('action');
-		var dateTo = req.param('dateTo');
-		var dateFrom = req.param('dateFrom');
+		var dateToStr = req.param('dateTo');
+		var dateFromStr = req.param('dateFrom');
+		dateFrom = moment(dateFromStr, "MM/DD/YYYY").startOf('day').toDate();
+    dateTo = moment(dateToStr, "MM/DD/YYYY").endOf('day').toDate();
+		log.findOne({createdAt: {'>=': dateFrom, '<': dateTo}}).exec(function(err, resultSet){
+			res.json(resultsSet);
+			return;
+		});
+
 
 
 	}
