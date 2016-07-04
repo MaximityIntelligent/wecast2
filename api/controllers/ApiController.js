7a1e99d52e2298f0d37ae37fbda38bbcde0652e3
KitChang <rdjk287@gmail.com>
2016-07-04 16:27:55 +0800

    vote exp api

--- api/controllers/ApiController.js
+++ api/controllers/ApiController.js
@@ -331,7 +331,7 @@ module.exports = {
     var ad = req.param('ad');
     var userVote = req.param('userVote');
     var now = new Date();
-    var exp = new Date('2016-07-10T19:00:00');
+    var exp = new Date('2016-07-11T03:00:00+0800');
     console.log(exp);
     if (now.getTime() > exp.getTime()) {
       return res.status(400).json({errCode: 0, errMsg:'投票時限已過了。'});
